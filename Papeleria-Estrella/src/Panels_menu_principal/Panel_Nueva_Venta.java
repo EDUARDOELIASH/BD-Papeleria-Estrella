d64f9df079b79b587c8a967fbf129c7d55ee9820
Eduardo-Hernande <63567836+Eduardo-Hernande@users.noreply.github.com>
2021-05-15 15:31:58 -0500

    Panel_Nueva_Ventana
    Se crea panel nueva ventana

---
+++ Papeleria-Estrella/src/Panels_menu_principal/Panel_Nueva_Venta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Panels_menu_principal;
+
+import javax.swing.JPanel;
+/**
+ *
+ * @author Eduardo Elias Hernandez Moreno
+ */
+public class Panel_Nueva_Venta extends JPanel{
+    //Talves se necesite parametro Connection
+    public Panel_Nueva_Venta (){
+    
+    }
+}
